238389188f87ff2d63b65c6a47ad8b0e08dcd327
Manuvel Victor <81816979+ManuvelVictor@users.noreply.github.com>
2023-01-26 01:13:18 +0530

    Operators

--- src/Day_3/Operators.java
+++ src/Day_3/Operators.java
@@ -2,6 +2,46 @@ package Day_3;
 
 public class Operators {
     public static void main(String[] args) {
-        //
+        /*
+        1.Arithmetic Operators
+        2.Unary Operators
+        3.Assignment Operators
+        4.Relational Operators
+        5.Logical Operators
+        6.Ternary Operators
+        7.Bitwise Operators
+        8.Shift Operators
+        9.Instance of Operator
+         */
+
+        /*
+        1.Arithmetic Operators
+        * : Multiplication
+        / : Division
+        % : Modulo
+        + : Addition
+        – : Subtraction
+         */
+
+        int a = 10;
+        int b = 20;
+        int multiply = a * b;
+        int division = a / b;
+        int modulo = a % b;
+        int add = a + b;
+        int sub = b - a;
+
+        /*
+        2.Unary Operators
+        – : Unary minus
+        + : Unary plus
+        ++ : Increment operator
+            Post-Increment
+            Pre-Increment
+        — : Decrement operator
+            Post-decrement
+            Pre-decrement
+        ! : Logical not operator
+         */
     }
 }
